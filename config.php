56e9fcc1ce77fafd18ec025842424f757106e57c
mariamsameh1234 <engysameh199@gmail.com>
2025-03-04 13:09:24 -0800

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 
 <?php
+
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '123456');
@@ -8,27 +9,16 @@ define('DB_NAME', 'caffe');
 class DatabaseConfig {
     private $host, $user, $pass, $dbname;
 
-    public function __construct($host, $user, $pass, $dbname) {
-        $this->host = $host;
-        $this->user = $user;
-        $this->pass = $pass;
-        $this->dbname = $dbname;
+    public function __construct() {
+        $this->host = DB_HOST;
+        $this->user = DB_USER;
+        $this->pass = DB_PASS;
+        $this->dbname = DB_NAME;
     }
 
-    public function getDbHost() {
-        return $this->host;
-    }
-
-    public function getUser() {
-        return $this->user;
-    }
-
-    public function getPass() {
-        return $this->pass;
-    }
-
-    public function getDbName() {
-        return $this->dbname;
-    }
+    public function getDbHost() { return $this->host; }
+    public function getUser() { return $this->user; }
+    public function getPass() { return $this->pass; }
+    public function getDbName() { return $this->dbname; }
 }
 ?>
