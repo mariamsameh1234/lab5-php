50ecdcbf016c7fc7bb2f0fe6a2cbccc19b10d193
mariamsameh1234 <engysameh199@gmail.com>
2025-03-04 08:07:40 -0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,15 +1,13 @@
-<?php
 session_start();
 include 'database.php';
 
-
-$config = new DatabaseConfig ('localhost', 'root', '123456', 'caffe');
+$config = new DatabaseConfig('localhost', 'root', '123456', 'caffe');
 $db = new Database($config);
 $userdb = new User($db);
 
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
-    delete_user($pdo, $id);
+    $userdb->delete($id); 
 }
 
 header("Location: display_users.php");
