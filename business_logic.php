2f9395ae13e050b302616b797763148fed2b1396
mariamsameh1234 <engysameh199@gmail.com>
2025-03-04 07:19:31 -0800

    Update business_logic.php

--- business_logic.php
+++ business_logic.php
@@ -1,4 +1,3 @@
-
 <?php
 
 class User {
@@ -8,33 +7,40 @@ class User {
         $this->db = $db;
     }
 
-   
     public function insert($name, $email, $password, $room_id, $ext, $profile_picture) {
         $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-        return $this->db->insert("user", ["name", "email", "password", "room_id", "ext", "profile_picture"], 
+        return $this->db->insert("users", ["name", "email", "password", "room_id", "ext", "profile_picture"], 
             [$name, $email, $hashed_password, $room_id, $ext, $profile_picture]);
     }
 
- 
     public function getAllRooms() {
         return $this->db->selectAll("rooms");
     }
 
-
     public function getAll() {
-        return $this->db->select("user LEFT JOIN rooms ON user.room_id = rooms.id", 
-            ["user.*", "rooms.name AS room_name"]);
+        return $this->db->select("users LEFT JOIN rooms ON users.room_id = rooms.id", 
+            ["users.*", "rooms.name AS room_name"]);
     }
 
-   
+    
     public function update($id, $name, $email, $room_id, $ext) {
-        return $this->db->update("user", ["name", "email", "room_id", "ext"], 
-            [$name, $email, $room_id, $ext, $id], "id = ?");
+        try {
+            $sql = "UPDATE users SET name = :name, email = :email, room_id = :room_id, ext = :ext WHERE id = :id";
+            $stmt = $this->db->pdo->prepare($sql);
+            return $stmt->execute([
+                ':name' => $name,
+                ':email' => $email,
+                ':room_id' => $room_id,
+                ':ext' => $ext,
+                ':id' => $id
+            ]);
+        } catch (PDOException $e) {
+            die("Error updating user: " . $e->getMessage());
+        }
     }
 
-   
     public function delete($id) {
-        return $this->db->delete("user", "id = ?", [$id]);
+        return $this->db->delete("users", "id = ?", [$id]);
     }
 }
 ?>
