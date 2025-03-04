787f8f30c371408130d5875775ca01f547cc9798
mariamsameh1234 <engysameh199@gmail.com>
2025-03-03 23:55:26 -0800

    Update config.php

--- config.php
+++ config.php
@@ -4,18 +4,18 @@ final class DatabaseConfig {
     private $user;
     private $pass;
     private $dbname;
-    
+    private $charset;
 
-    public function __construct($host, $user, $pass, $dbname) {
+    public function __construct($host, $user, $pass, $dbname, $charset = "utf8mb4") {
         $this->host = $host;
         $this->user = $user;
         $this->pass = $pass;
         $this->dbname = $dbname;
-        
+        $this->charset = $charset;
     }
 
     public function getDsn() {
-        return "mysql:host=$this->host;dbname=$this->dbname";
+        return "mysql:host=$this->host;dbname=$this->dbname;charset=$this->charset";
     }
 
     public function getUser() {
