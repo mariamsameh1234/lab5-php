820aaa7133979ad0dd8879e897eaa23d49073ade
mariamsameh1234 <engysameh199@gmail.com>
2025-03-08 00:35:57 -0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,17 +1,26 @@
 <?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
-session_start();
-include 'database.php';
+include 'config.php'; 
+include 'datebase.php'; 
+include 'user.php'; 
 
-$config = new DatabaseConfig('localhost', 'root', '123456', 'caffe');
-$db = new Database($config);
-$userdb = new User($db);
+$db = new Database(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$userObj = new User($db);
+
 
 if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    $userdb->delete($id); 
+    $userId = $_GET['id'];
+    
+   
+    if ($userObj->delete($userId)) {
+        header("Location: display_users.php?success=User deleted successfully");
+    } else {
+        header("Location: display_users.php?error=Failed to delete user");
+    }
+    exit;
 }
-
-header("Location: display_users.php");
-exit();
 ?>
+
